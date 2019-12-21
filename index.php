4f8cfef25b02596a4717d6767c805d0ebcd07e19
vanshkambioj <vanshkambioj.mca@gmail.com>
2019-12-21 22:34:30 +0530

    maimt server web

---
+++ index.php
@@ -0,0 +1,145 @@
+<?php
+include("header.html");
+?>
+
+                                  <!--================== start background image part=======================-->
+                        <div class="image">
+                            <div class="overlay">
+                                <div class="wrap">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <h1><span class="display-4">Welcome To</span> <br><span class="word">MAHARAJA </span>AGRASEN INSTITUTE</h1>
+                                            <h2>of</h2>
+                                            <h1>Management & Technology</h1>
+                                            <h1 class="display-1" style="font-size:92px; font-weight:500;font-family:myfont;letter-spacing:3px; background: -webkit-linear-gradient(#03b0c5, #d3fcfc);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;">Digital Library </h1>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+            <!--==================end background image part=======================-->
+            <!--==================start  about areat=======================-->
+            <section class="section about mt-5">
+                <div class="container">
+                    <div class="single-about row">
+                        <div class="col-lg-4 col-md-6">
+                            <div class="about-content">
+                                <h1>Purpose of <br> Digital Library @ MAIMT</h1>
+                                <p>The Digital Library at Maharaja Agrasen Institute of Management & Technology has been developed to provide easy access of the resources like E-book, Journal, Syllabus, Previous Years University Questions, Time Table and other academic resources to the student of the institute. </p>
+                                <a href="" class="primary-btn">Details</a>
+                            </div>
+                        </div>
+                        <div class="col-lg-7 col-md-6">
+                            <div class="about-thumb">
+                                <img src="images/book_logo.jpg" class="img-fluid info-img" alt="book_logo" height="100vh">
+                            </div>
+                        </div>
+                        <div class="bordered-img">
+                            <img src="images/laptop_logo.jpg" class=" img-fluid info-img" alt="laptop_logo">
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!--==================end about area=======================-->
+            <!--==================start facility=======================-->
+           <div class="facility pb-5">
+                <div class="container">
+                <div class="row mt-5 pt-3">
+                    <div class="col-md-12 text-center">
+                        <h1 class="text-light">E-Content</h1>
+                    </div>
+                </div>
+                <div class="row text-center mt-3">
+                    <div class="col-md-4">
+                    <a href="econtent/elearningsem.php"><div class="facility-box">
+                            <div class="facility-icon mb-3">
+                                <span class="ico-circle">
+                                    <img class="img" src="images/bca_logo.png" alt="bca_logo">
+                                </span>
+                            </div>
+                            <div class="facility-content pb-2">
+                                <h4>BCA</h4>
+                                <!-- <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet quis et magnam?</p> -->
+                            </div>
+                        </div>
+                    </div></a>
+                    <div class="col-md-4">
+                            <div class="facility-box">
+                            <a href="econtent/elearningsem.php">  <div class="facility-icon mb-3">
+                                    <span class="ico-circle">
+                                        <img class="img" src="images/bba_logo.png" alt="bba_logo">
+                                    </span>
+                                </div>
+                                <div class="facility-content pb-2">
+                                    <h4>BBA</h4>
+                                    <!-- <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet quis et magnam?</p> -->
+                                </div>
+                            </div>
+                        </div></a>
+                        <div class="col-md-4">
+                        <a href="econtent/elearningsem.php"> <div class="facility-box">
+                                    <div class="facility-icon mb-3">
+                                        <span class="ico-circle">
+                                            <img class="img" src="images/mca_logo.png" alt="mca_logo">
+                                        </span>
+                                    </div>
+                                    <div class="facility-content pb-2">
+                                        <h4>MCA</h4>
+                                        <!-- <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet quis et magnam?</p> -->
+                                    </div>
+                                </div>
+                            </div></a>
+                </div>
+                <div class="row text-center mt-3">
+                    <div class="col-md-4 mt-3">
+                        <div class="facility-box">
+                        <a href="econtent/elearningsem.php"> <div class="facility-icon mb-3">
+                                <span class="ico-circle">
+                                    <img class="img" src="images/mba_logo.png" alt="mba_logo">
+                                </span>
+                            </div>
+                            <div class="facility-content pb-2">
+                                <h4>MBA</h4>
+                                <!-- <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet quis et magnam?</p> -->
+                            </div>
+                        </div>
+                    </div></a>
+                    <div class="col-md-4 mt-3">
+                    <a href="econtent/elearningsem.php">  <div class="facility-box">
+                                <div class="facility-icon mb-3">
+                                    <span class="ico-circle">
+                                        <img class="img" src="images/management_logo.png" alt="management_logo">
+                                    </span>
+                                </div>
+                                <div class="facility-content pb-2">
+                                    <h4>VALUE ADDED COURSES FOR MANAGEMENT</h4>
+                                    <!-- <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet quis et magnam?</p> -->
+                                </div>
+                            </div>
+                        </div></a>
+                        <div class="col-md-4 mt-3">
+                        <a href="econtent/elearningsem.php">   <div class="facility-box">
+                                    <div class="facility-icon mb-3">
+                                        <span class="ico-circle">
+                                            <img class="img" src="images/computer_application_logo.png" alt="computer_application_logo">
+                                        </span>
+                                    </div>
+                                    <div class="facility-content pb-2">
+                                        <h4>VALUE ADDED COURSES FOR COMPUTER APPLICATION</h4>
+                                        <!-- <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet quis et magnam?</p> -->
+                                    </div>
+                                </div>
+                            </div></a>
+                </div>
+                </div>
+            </div>            <!--==================end facility=======================-->
+            
+    
+    <?php
+include("footer.html");
+?>
+        
+           
